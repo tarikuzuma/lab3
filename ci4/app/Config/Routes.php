e01a3b70eb3371f906909591a250a4da73677758
Tarikuzuma <edwinjrgumba@gmail.com>
2024-02-27 18:36:18 +0800

    Added News Creator File

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Controllers\News; // Add this line
+use App\Controllers\News; 
 use App\Controllers\Pages;
 
-$routes->get('news', [News::class, 'index']);           // Add this line
-$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+$routes->get('news', [News::class, 'index']);  
+$routes->get('news/new', [News::class, 'new']); 
+$routes->post('news', [News::class, 'create']);     
+$routes->get('news/(:segment)', [News::class, 'show']); 
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
