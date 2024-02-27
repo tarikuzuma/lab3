c436b191ba2e152595e23e81bf4e7ac69033db04
Tarikuzuma <edwinjrgumba@gmail.com>
2024-02-27 17:55:51 +0800

    Added News 1

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -1,12 +1,10 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
-
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index');
+use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
