17bfd7320b7db60b5213d4e40bb672e4ad35a5e6
Tarikuzuma <edwinjrgumba@gmail.com>
2024-03-05 20:25:14 +0800

    chore: fixed routing

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -2,7 +2,7 @@
 
 use App\Controllers\News; 
 use App\Controllers\Pages;
-use App\Controllers\Project;   
+use App\Controllers\ProjectController;   
 
 $routes->get('news', [News::class, 'index']);  
 $routes->get('news/new', [News::class, 'new']); 
@@ -10,14 +10,4 @@ $routes->post('news', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'show']); 
 
 $routes->get('pages', [Pages::class, 'index']);
-$routes->get('(:segment)', [Pages::class, 'view']);
-
-$routes->get('pages', [Pages::class, 'projects']);
-$routes->get('(:segment)', [Pages::class, 'view']);
-
-$routes->get('projects', 'Project::index'); // Route for projects
-$routes->get('projects/personal', 'Project::personal'); // Route for personal projects
-$routes->get('projects/academic', 'Project::academic'); // Route for academic projects
-
-$routes->get('pages', [Pages::class, 'hobbies']);
 $routes->get('(:segment)', [Pages::class, 'view']);
---
+++ ci4/app/Views/pages/personal_project.php
@@ -0,0 +1,225 @@
+      <!-- Second Section of Page -->
+      <section class="hidden">
+        <div class="main_top" style="height: 75%; margin-top: 10%; margin-bottom: 10%;">
+            <div class="text" style="font-size: 80px;">
+                <span class="drop_word">Personal Projects</span>
+            </div>
+            <div class="sub_text drop_word delayed_drop_word">
+                Projects that fix inconeviences in my life, or perhaps just to practice programming.
+            </div>
+        </div>
+      </section>
+
+      <!-- Project 1 -->
+      <section class="hidden project_section">
+        <div class="project_content">
+
+
+            
+            <div class="container">
+                <div class="proj_title">
+                  <a class = "github_link" href = "https://github.com/tarikuzuma/Bootcamp-3T2021-SHS-Fursuit-of-Justice">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                    </svg>           
+                    Gestaltzerfall
+                  </a>
+                </div>
+
+                <div class="about">
+                    Open Sourced Unity Game wherein you act as a ghost player eradicating evil to save your fellow NPC friends!<br><br>
+
+                    Languages: C# <br>
+                    Softwares Used: Unity (2022), Figma, Visual Studio Code, Photoshop, GitHub<br>
+                </div>
+
+                
+            <div class = "container">
+              <div class="carousel" data-carousel>
+                <button class="carousel-button prev" data-carousel-button="prev">&#8656;</button>
+                <button class="carousel-button next" data-carousel-button="next">&#8658;</button>
+                <ul data-slides>
+                  <li class="slide" data-active>
+                    <img src="https://source.unsplash.com/78A265wPiO4" alt="Nature Image #1">
+                  </li>
+                  <li class="slide">
+                    <img src="https://source.unsplash.com/eOpewngf68w" alt="Nature Image #2">
+                  </li>
+                  <li class="slide">
+                    <img src="https://source.unsplash.com/ndN00KmbJ1c" alt="Nature Image #3">
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+
+                <div class="attributes">
+                  <div class="tag tag1 hidden2">
+                      Unity
+                  </div>
+                  <div class="tag tag2 hidden2">
+                      Game Development!
+                  </div>
+                  <div class="tag tag3 hidden2">
+                      Platformer
+                  </div>
+              </div>
+              
+            </div>
+            
+        </div>
+      </section>
+
+      <!-- Project 2 -->
+      <section class="hidden project_section">
+        <div class="project_content">
+
+
+            
+            <div class="container">
+                <div class="proj_title">
+                  <a class = "github_link" href = "https://github.com/tarikuzuma/Bootcamp-3T2021-SHS-Fursuit-of-Justice">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                    </svg>           
+                  ArchiType (2023)
+                  </a>
+                </div>
+
+                <div class="about">
+                    A compilation of ready to use calculators caterd to fit the needs of an architecture student.
+                </div>
+
+                
+            <div class = "container">
+              <div class="carousel" data-carousel>
+                <button class="carousel-button prev" data-carousel-button="prev">&#8656;</button>
+                <button class="carousel-button next" data-carousel-button="next">&#8658;</button>
+                <ul data-slides>
+                  <li class="slide" data-active>
+                    <img src="https://source.unsplash.com/78A265wPiO4" alt="Nature Image #1">
+                  </li>
+                  <li class="slide">
+                    <img src="https://source.unsplash.com/eOpewngf68w" alt="Nature Image #2">
+                  </li>
+                  <li class="slide">
+                    <img src="https://source.unsplash.com/ndN00KmbJ1c" alt="Nature Image #3">
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+
+                <div class="attributes">
+                  <div class="tag tag1 hidden2">
+                      100% Python
+                  </div>
+                  <div class="tag tag2 hidden2">
+                      Architecture
+                  </div>
+                  <div class="tag tag3 hidden2">
+                      Calculators
+                  </div>
+              </div>
+              
+            </div>
+            
+        </div>
+      </section>
+
+      <!-- Project 2 -->
+      <section class="hidden project_section">
+        <div class="project_content">
+
+
+            
+            <div class="container">
+                <div class="proj_title">
+                  <a class = "github_link" href = "https://github.com/tarikuzuma/Bootcamp-3T2021-SHS-Fursuit-of-Justice">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                    </svg>           
+                  Project 1 
+                  </a>
+                </div>
+
+                <div class="about">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies aliquam,
+                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
+                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
+                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
+                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
+                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
+                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc.
+                </div>
+
+                
+            <div class = "container">
+              <div class="carousel" data-carousel>
+                <button class="carousel-button prev" data-carousel-button="prev">&#8656;</button>
+                <button class="carousel-button next" data-carousel-button="next">&#8658;</button>
+                <ul data-slides>
+                  <li class="slide" data-active>
+                    <img src="https://source.unsplash.com/78A265wPiO4" alt="Nature Image #1">
+                  </li>
+                  <li class="slide">
+                    <img src="https://source.unsplash.com/eOpewngf68w" alt="Nature Image #2">
+                  </li>
+                  <li class="slide">
+                    <img src="https://source.unsplash.com/ndN00KmbJ1c" alt="Nature Image #3">
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+
+                <div class="attributes">
+                  <div class="tag tag1 hidden2">
+                      100% Python & RenPy
+                  </div>
+                  <div class="tag tag2 hidden2">
+                      Visual Novel
+                  </div>
+                  <div class="tag tag3 hidden2">
+                      Lawyer-Centric!
+                  </div>
+              </div>
+              
+            </div>
+            
+        </div>
+      </section>
+
+      
+      <section class = "hidden">
+        <div class = "cards_projects_main">
+          <div class = "row">
+            <a href="academic_project.php" class="card hidden2" style="transition: 2s;">
+              <div class="img-avatar">
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+                  <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                </svg>
+              </div>
+              <div class="card-text">
+
+                 <div class="portada">
+                  <img src="assets/projects/apc-building.jpg" alt="Academic Project">
+                 </div>
+
+                 <div class="title-total">
+                    <div class="title">Project 1</div>
+                    <h2>Academic Projects</h2>
+                    <div class="desc">All of the Academic Projects I've compiled over the past few years.
+                      From Senior Highschool to College!
+                    </div>
+                    <div class="actions">
+                       <button><i class="far fa-heart"></i></button>
+                       <button><i class="far fa-envelope"></i></button>
+                       <button><i class="fas fa-user-friends"></i></button>
+                    </div>
+                 </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </section>
