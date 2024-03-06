5ffaa0761ed804c718edcbd2de7f5f06aa75f329
Tarikuzuma <edwinjrgumba@gmail.com>
2024-03-06 22:54:17 +0800

    chore: Fixed Lightroom cringe

--- ci4/app/Views/templates/header_hobbies.php
+++ ci4/app/Views/templates/header_hobbies.php
@@ -9,7 +9,7 @@
 
     <link rel = "stylesheet" type = "text/css" href = "css/hobbies.css">
     <link rel="stylesheet" type = "text/css" href = "css/contact_cards.css">
-    <link rel="stylesheet" type = "text/css" href = "css/lightbox.min.css">
+    <link rel="stylesheet" type = "text/css" href = "lightbox.min.css">
     <link rel="stylesheet" type = "text/css" href = "css/gallery.css">
 
     <script defer src = "js/app.js"></script>
